a8b49720a4b78efcf2b78f827161907e7d9057c6
tarikerbay <tarikerbay@air-von-tarik.fritz.box>
2022-03-24 17:40:00 +0100

    Testing

---
+++ TestProjekt/src/testing/Hallo.java
@@ -0,0 +1,14 @@
+package testing;
+
+public class Hallo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int b = 10;
+		
+		System.out.println((a+b));
+
+	}
+
+}
